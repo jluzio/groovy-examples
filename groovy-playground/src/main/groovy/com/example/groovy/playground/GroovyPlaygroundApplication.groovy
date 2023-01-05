3527c4d08e7c8c130b8d6d0b449c1136bd6ad6b8
João Luzio <joao.luzio@gmail.com>
2023-01-05 17:32:05 +0000

    Initial sample

---
+++ groovy-playground/src/main/groovy/com/example/groovy/playground/GroovyPlaygroundApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.groovy.playground
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GroovyPlaygroundApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(GroovyPlaygroundApplication, args)
+  }
+
+}
---
+++ groovy-playground/src/test/groovy/com/example/groovy/playground/GroovyPlaygroundApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.groovy.playground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GroovyPlaygroundApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
