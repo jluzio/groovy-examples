680e60a0ccd9f35db2f358d5bc0052d8c3713857
João Luzio <joao.luzio@gmail.com>
2023-03-23 14:34:07 +0000

    Examples

--- groovy-playground/src/test/groovy/com/example/groovy/playground/lang/MethodParametersTest.groovy
+++ groovy-playground/src/test/groovy/com/example/groovy/playground/lang/MethodParametersTest.groovy
@@ -4,7 +4,6 @@ import groovy.util.logging.Slf4j
 import org.junit.jupiter.api.Test
 
 import java.util.function.Consumer
-import java.util.function.Function
 
 import static org.assertj.core.api.Assertions.assertThat
 
@@ -21,15 +20,32 @@ class MethodParametersTest {
       "Hello ${target ?: 'default'}!"
     }
 
+    def multipleDefaultParams(var1 = "val1", var2 = "val2", var3, var4) {
+      [var1: var1, var2: var2, var3: var3, var4: var4]
+    }
+
+    def multipleDefaultParamsMixedTypes(String var1 = "val1", Integer var2 = 2, String var3, Integer var4) {
+      [var1: var1, var2: var2, var3: var3, var4: var4]
+    }
+
+    def namedParamsConvention(Map args, Integer var2 = 2) {
+      args + [var2: var2]
+    }
+
   }
 
+  def tester = new TestBean()
+  Consumer<Object> logIt = v -> log.debug("{}", v)
+
   @Test
-  void test() {
-    def tester = new TestBean()
-    Consumer<Object> logIt = v -> log.debug("{}", v)
+  void test_defaultParams() {
     assertThat(tester.defaultParams())
         .satisfies(logIt)
         .isEqualTo("Hello world!")
+  }
+
+  @Test
+  void test_optional() {
     assertThat(tester.optional())
         .satisfies(logIt)
         .isEqualTo("Hello default!")
@@ -38,5 +54,43 @@ class MethodParametersTest {
         .isEqualTo("Hello opt!")
   }
 
+  @Test
+  void test_multipleDefaultParams() {
+    assertThat(tester.multipleDefaultParams("val3", "val4"))
+        .satisfies(logIt)
+        .isEqualTo([var1: "val1", var2: "val2", var3: "val3", var4: "val4"])
+    assertThat(tester.multipleDefaultParams("custom_val1", "val3", "val4"))
+        .satisfies(logIt)
+        .isEqualTo([var1: "custom_val1", var2: "val2", var3: "val3", var4: "val4"])
+    assertThat(tester.multipleDefaultParams("custom_val1", "custom_val2", "val3", "val4"))
+        .satisfies(logIt)
+        .isEqualTo([var1: "custom_val1", var2: "custom_val2", var3: "val3", var4: "val4"])
+  }
+
+  @Test
+  void test_multipleDefaultParamsMixedTypes() {
+    assertThat(tester.multipleDefaultParamsMixedTypes("val3", 4))
+        .satisfies(logIt)
+        .isEqualTo([var1: "val1", var2: 2, var3: "val3", var4: 4])
+    assertThat(tester.multipleDefaultParamsMixedTypes("custom_val1", "val3", 4))
+        .satisfies(logIt)
+        .isEqualTo([var1: "custom_val1", var2: 2, var3: "val3", var4: 4])
+    assertThat(tester.multipleDefaultParamsMixedTypes("custom_val1", 222, "val3", 4))
+        .satisfies(logIt)
+        .isEqualTo([var1: "custom_val1", var2: 222, var3: "val3", var4: 4])
+  }
+
+  @Test
+  void test_namedParamsConvention() {
+    assertThat(tester.namedParamsConvention(mapVar1: 1, mapVar2: 2, 2))
+        .satisfies(logIt)
+        .isEqualTo([mapVar1: 1, mapVar2: 2, var2: 2])
+
+    def initialArgs = [mapVar1: 1, mapVar2: 2]
+    assertThat(tester.namedParamsConvention(initialArgs, 2))
+        .satisfies(logIt)
+        .isEqualTo([mapVar1: 1, mapVar2: 2, var2: 2])
+        .isNotEqualTo(initialArgs)
+  }
 
 }
---
+++ groovy-playground/src/test/groovy/com/example/groovy/playground/lang/StringTest.groovy
@@ -0,0 +1,16 @@
+package com.example.groovy.playground.lang
+
+import org.junit.jupiter.api.Test
+
+import static org.assertj.core.api.Assertions.assertThat
+
+class StringTest {
+
+  @Test
+  void join() {
+    def values = ["a", "b", "c"]
+    def output = values.collect {"!" + it }.join(",")
+    assertThat(output).isEqualTo("!a,!b,!c")
+  }
+
+}
